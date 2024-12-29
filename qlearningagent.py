639ccb2aef20c57aa219ec38dee4426fdd1f9720
Akshay Gulabrao <aksgula22@gmail.com>
2024-12-29 06:58:54 -0800

    fix: use config.yaml file for qlearningagent.py
    Instead of using keyword arguments for functions in qlearningagent.py,
    I use config/config.yaml file for quicker and simpler control flow,
    and parameter changes. This should make running experiments significantly
    more painless as now they simply involve editing yaml files instead of python
    files.

--- qlearningagent.py
+++ qlearningagent.py
@@ -1,44 +1,46 @@
-import os
 import gymnasium as gym
-import flappy_bird_gymnasium
-from stable_baselines3 import DQN
+import flappy_bird_gymnasium 
+import stable_baselines3 as sb3
+import yaml
+
+with open("config/config.yaml", "r") as file:
+    config = yaml.safe_load(file)
 
 
 
-env = gym.make(
-    "FlappyBird-v0",
-    audio_on=True,
-    use_lidar=False,
-    normalize_obs=True,
-    score_limit=20,
-    pipe_gap=100,
-)
-model = DQN.load("flappybird_dqn", env=env)
+env = gym.make("FlappyBird-v0", use_lidar=False)
 
-model.learn(total_timesteps=1e6, log_interval=1e3)
+if config["train-from-scratch"]:
+    model = sb3.DQN(policy="MlpPolicy",
+        env=env,
+        learning_rate=config["model"]["learning_rate"],
+        buffer_size=config["model"]["buffer_size"],  # 1e6
+        learning_starts=config["model"]["learning_starts"],
+        batch_size=config["model"]["batch_size"],
+        tau=config["model"]["tau"],
+        gamma= config["model"]["gamma"],
+        train_freq=config["model"]["train_freq"],
+        gradient_steps=config["model"]["gradient_steps"],
+        replay_buffer_class=config["model"]["replay_buffer_class"],
+        replay_buffer_kwargs=config["model"]["replay_buffer_kwargs"],
+        optimize_memory_usage=config["model"]["optimize_memory_usage"],
+        target_update_interval=config["model"]["target_update_interval"],
+        exploration_fraction=config["model"]["exploration_fraction"],
+        exploration_initial_eps=config["model"]["exploration_initial_eps"],
+        exploration_final_eps=config["model"]["exploration_final_eps"],
+        max_grad_norm=config["model"]["max_grad_norm"],
+        stats_window_size=config["model"]["stats_window_size"],
+        tensorboard_log=config["model"]["tensorboard_log"],
+        policy_kwargs=config["model"]["policy_kwargs"],
+        verbose=config["model"]["verbose"],
+        seed=config["model"]["seed"],
+        device=config["model"]["device"],
+        _init_setup_model=config["model"]["_init_setup_model"])
 
+    model.learn(total_timesteps=config["total_timesteps"], 
+                tb_log_name=config["tb_log_name"],
+                reset_num_timesteps=config["reset_num_timesteps"])
 
-
-
-model.save("flappybird_dqn")
-
-
-
-env = gym.make(
-        "FlappyBird-v0",
-        audio_on=True,
-        use_lidar=False,
-        render_mode="human",
-        normalize_obs=True,
-        score_limit=20,
-        pipe_gap=100,
-    )
-model = DQN.load("flappybird_dqn", env=env)
-
-obs, info = env.reset()
-while True:
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, terminated, truncated, info = env.step(action)
-    if terminated or truncated:
-        obs, info = env.reset()
-        break
+    model.save("dqn_flappy_bird")
+else:
+    model = sb3.DQN.load("dqn_flappy_bird")
