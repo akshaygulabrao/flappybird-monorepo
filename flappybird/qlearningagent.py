c53be0901b5e2e1c45624f6941d01baaf26c6a80
Akshay Gulabrao <aksgula22@gmail.com>
2024-12-30 00:11:08 -0800

    feat: new callback for checking scores during eval
    This commit introduces  example implementation of using a custom callback to compute
    the average score over n training runs. In this case it is 3.
    
    Key Changes:
    - New ScoreCallback class with private 'scores' attribute to store results
    - Implemented the '__call__' method which appends scores to the
    private attriute when invoked in the callback fn.
    - Integrated the ScoreCallback instance into the evaluate_policy fn to track
    scores with the scores attribute

--- flappybird/qlearningagent.py
+++ flappybird/qlearningagent.py
@@ -1,52 +1,41 @@
+"""
+Q-Learning agent for flappy bird. Uses simplified observation space of 12 features.
+
+Uses stable-baseline3 DQN implementation. Uses wandb for logging. Used for benchmarking when model
+improvement stops. Running 10 million episodes, and storing evaluation metrics every million
+episodes.
+"""
 import gymnasium as gym
 import numpy as np
 import stable_baselines3 as sb3
 import yaml
 import flappy_bird_env
+
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.callbacks import EvalCallback
+import wandb
 
+assert flappy_bird_env is not None, "flappy_bird_env is not installed"
 
-assert flappy_bird_env
 with open("config/config.yaml", "r",encoding="utf-8") as file:
     config = yaml.safe_load(file)
 
 
-if config["train"]:
-    env = gym.make("FlappyBird-v0", use_lidar=False)
-    env = Monitor(env, "logs/")
+env = gym.make("FlappyBird-v0", use_lidar=False)
+env = Monitor(env, "logs/")
 
-    if config["train-from-scratch"]:
-        model = sb3.DQN(policy="MlpPolicy",
-            env=env,
-            **config["model"])
-    else:
-        model = sb3.DQN.load("dqn_flappy_bird",env=env)
-
-    model.learn(**config["training-run"],
-                progress_bar=True)
-
-    model.save("dqn_flappy_bird")
-
-else:
-    episode_rewards = []
-    final_scores = []
-    def callback(locals_, globals_):
-        if locals_['done']:
-            final_scores.append(locals_['info']['score'])
+class ScoreCallback:
+    def __init__(self):
+        self.scores = []    
+    def __call__(self, locals_dict, globals_dict):
+        if locals_dict['dones'][0]:
+            self.scores.append(locals_dict['infos'][0]['score'])
         return True
-    
-    env = gym.make("FlappyBird-v0", use_lidar=False)
-    env = Monitor(env, "logs/")
-    model = sb3.DQN.load("dqn_flappy_bird",env=env)
-    # Evaluate the model
-    mean_reward, std_reward = evaluate_policy(
-        model, 
-        env, 
-        n_eval_episodes=100,
-        callback=callback,
-        deterministic=True,
-    )
-    print(f"Mean reward: {mean_reward:.2f} +/- {std_reward:.2f}")
-    print(f"Final scores: {np.mean(final_scores)}")
 
+model = sb3.DQN(policy="MlpPolicy",
+    env=env,
+    **config["model"])
+
+score_callback = ScoreCallback()    
+print(evaluate_policy(model, env, n_eval_episodes=3, deterministic=True, callback=score_callback)) 
